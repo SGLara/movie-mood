27b9c0662befdb9f9d63df6d948d16d80a321a75
Steven Lara <steven.gustavo.lara@gmail.com>
2024-07-25 23:29:21 -0600

    :heavy_plus_sign: add dependencies

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
+import animations from '@midudev/tailwind-animations'
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [animations]
 }
