e086240b15386994ec9674aa064251cf344fb95b
Steven Lara <steven.gustavo.lara@gmail.com>
2024-08-04 10:47:40 -0600

    Add types for api

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 declare module '*.png'
+
+interface ImportMetaEnv {
+  readonly PUBLIC_TMDB_API_URL: string
+  readonly PUBLIC_TMDB_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
