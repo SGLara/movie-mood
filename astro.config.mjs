78f5c0a8b408777ae085eb1120042745ed599295
Steven Lara <steven.gustavo.lara@gmail.com>
2024-09-25 22:53:21 -0600

    :heavy_plus_sign: chore: add missing vercel adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
+import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  prefetch: true
+  prefetch: true,
+  adapter: vercel()
 })
