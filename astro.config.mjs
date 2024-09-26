43e27ce49593f15e6bd2a11de93ad231bfddcab6
Steven Lara <steven.gustavo.lara@gmail.com>
2024-09-25 19:11:59 -0600

    Change SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'hybrid',
+  output: 'server',
   prefetch: true
 })
